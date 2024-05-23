70b4cd1bfb063a0a0c945b406f23465fed3cb33e
MassimilianoRocco <massimilianorocco93@gmail.com>
2024-05-23 17:29:24 +0200

    Added: header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,38 @@
 <!-- SCRIPT GENERALE -->
 <script>
-import Header from './components/Header.vue'
-import Main from './components/Main.vue'
-import Footer from './components/Footer.vue'
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  components: {
+    Header,
+    Main,
+    Footer,
+  },
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    
+  },
+  mounted() {
+    
+  }
+}
 </script>
 
 
 
 <!-- TEMPLATE HTML GENERALE -->
 <template>
+  
+  <Header />
+
+
+
   
 </template>
 
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,53 @@
 <!-- SCRIPT -->
 <script>
-
+export default {
+    data() {
+        return {
+            links: [
+                {
+                    text: "CHARACTERS",
+                    url: "#characters",
+                },
+                {
+                    text: "COMICS",
+                    url: "#comics",
+                },
+                {
+                    text: "MOVIES",
+                    url: "#movies",
+                },
+                {
+                    text: "TV",
+                    url: "#tv",
+                },
+                {
+                    text: "GAMES",
+                    url: "#games",
+                },
+                {
+                    text: "COLLECTIBLES",
+                    url: "#collectibles",
+                },
+                {
+                    text: "VIDEOS",
+                    url: "#videos",
+                },
+                {
+                    text: "FANS",
+                    url: "#fans",
+                },
+                {
+                    text: "NEWS",
+                    url: "#news",
+                },
+                {
+                    text: "SHOP",
+                    url: "#shop",
+                },
+            ],
+        }
+    }
+}
 
 </script>
 
@@ -8,6 +55,18 @@
 
 <!-- TEMPLATE HTML -->
 <template>
+  <div class="row bg-light justify-content-center">
+    <div class="col-8 d-flex py-2">
+      <div class="col-4">
+        <img class="logo" src="../assets/img/dc-logo.png" />
+      </div>
+      <div class="links_container col-8 d-flex justify-content-end align-items-center text-center">
+        <div class="header_links col-1 d-flex justify-content-center align-items-center" v-for="link in links">
+          {{ link.text }}
+        </div>
+      </div>
+    </div>
+  </div>
 
 </template>
 
@@ -15,5 +74,17 @@
 
 <!-- STYLE -->
 <style scoped>
+
+.header_links{
+  height: 100%;
+  width: calc(100% / 10);
+
+  cursor: pointer;
+  font-size: 0.7rem;
+  color: black;
+}
+.header_links:hover{
+  border-bottom: 5px solid blue;
+}
 
 </style>
